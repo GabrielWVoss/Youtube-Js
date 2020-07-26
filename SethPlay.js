4cdcda60a87db5e60a645ca02b9514ddda437e34
Isadora Rebelo <isadorarebelo@Isadoras-MacBook-Pro.local>
2020-07-26 23:49:22 +0200

    3

--- SethPlay.js
+++ SethPlay.js
@@ -1,17 +1,19 @@
 var player;
 
-const trackName = ["If You Want Love", "Stronger", "Need You Still", "Warriors", "Not Afraid"];
-const trackSinger = ["NF", "Kanye West", "Ivan B feat. Keith Fontano", "Eminem"];
+const trackName = ["If You Want Love", "Stronger", "Need You Still", "Warriors", "Not Afraid", "Calm Snow"];
+const trackSinger = ["NF", "Kanye West", "Ivan B feat. Keith Fontano", "Eminem", "I See Stars"];
 const trackCover = ["url(https://i.imgur.com/zmAR40Y.jpg)", 
                     "url(https://i.imgur.com/5BZ7vxg.jpg)", 
                     "url(https://i.imgur.com/bfKgV91.jpg)", 
                     "url(https://i.imgur.com/lOpDLkq.jpg)",
-                    "url(https://i.imgur.com/RRFrOq0.jpg)"];
+                    "url(https://i.imgur.com/RRFrOq0.jpg)",
+                    "url(https://i.imgur.com/RRFrOq0.jpg)" ];
 const trackBack = ["url(https://images2.imgbox.com/3e/e9/34oiP2E6_o.gif)", 
                     "url(https://i.imgur.com/CoXRvir.gif)", 
                     "url(https://images2.imgbox.com/27/96/lLrZ1Sg3_o.gif)", 
                     "url(https://i.imgur.com/I4j4SMS.gif)",
-                    "url(https://i.imgur.com/CIRAFDn.gif)"];					
+                    "url(https://i.imgur.com/CIRAFDn.gif)",
+                    "url(https://i.imgur.com/CIRAFDn.gif)" ];					
 let index = 0;
 
 function onYouTubeIframeAPIReady() {
@@ -21,7 +23,7 @@ function onYouTubeIframeAPIReady() {
         videoId: 'LfxY1sg5_rw',
         playerVars: {
             color: 'white',
-			playlist: 'PsO6ZnUZI0g,EVzUk-uk_Nc,o3W5ngVTtRE',
+			playlist: 'PsO6ZnUZI0g,EVzUk-uk_Nc,o3W5ngVTtRE,j5-yKhDd64s,LXvkrEwy0Xs',
 			controls: '0',
         },
         events: {
